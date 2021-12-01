c2eb904225e8fb5e77c60a8eaa1754827fdc1fbd
sitiaisyahk1 <sitiaisyahkg@gmail.com>
2021-12-01 22:06:56 +0700

    add feature timer

--- app/src/main/java/com/aisyah/learnapp/home/DetailModulesActivity.kt
+++ app/src/main/java/com/aisyah/learnapp/home/DetailModulesActivity.kt
@@ -2,7 +2,11 @@ package com.aisyah.learnapp.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.TextView
 import com.aisyah.learnapp.R
 import com.aisyah.learnapp.model.module.ModelModule
 import kotlinx.android.synthetic.main.activity_detail_modules.*
@@ -14,10 +18,33 @@ class DetailModulesActivity : AppCompatActivity() {
         const val EXTRA_MODULE = "extra_module"
     }
 
+    var handler: Handler? = null
+    var hour: TextView? = null
+    var minute: TextView? = null
+    var seconds: TextView? = null
+    var milli_seconds: TextView? = null
+
+    internal var MillisecondTime: Long = 0
+    internal var StartTime: Long = 0
+    internal var TimeBuff: Long = 0
+    internal var UpdateTime = 0L
+
+
+    internal var Seconds: Int = 0
+    internal var Minutes: Int = 0
+    internal var MilliSeconds: Int = 0
+
+    internal var flag:Boolean=false
+
+    var startButton: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_modules)
         setSupportActionBar(toolbar_module)
+
+        startButton = findViewById(R.id.btn_play)
+        bindViews()
 
         val module = intent.getParcelableExtra<ModelModule>(EXTRA_MODULE) as ModelModule
 
@@ -27,6 +54,61 @@ class DetailModulesActivity : AppCompatActivity() {
         tv_title_module.text = module.title
         tv_desc_module.text = module.description
         tv_link_module.text = module.linkModule
+    }
+
+    private fun bindViews() {
+        hour = findViewById(R.id.hour)
+        minute = findViewById(R.id.minute)
+        seconds = findViewById(R.id.seconds)
+
+
+        startButton?.setOnClickListener {
+            if (flag){
+                handler?.removeCallbacks(runnable)
+                startButton?.setImageResource(R.drawable.ic_play)
+                flag=false
+            }else{
+                startButton?.setImageResource(R.drawable.ic_reset)
+                StartTime = SystemClock.uptimeMillis()
+                handler?.postDelayed(runnable, 0)
+                flag=true
+            }
+        }
+
+        handler = Handler()
+    }
+
+    var runnable: Runnable = object : Runnable {
+
+        override fun run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime
+
+            UpdateTime = TimeBuff + MillisecondTime
+
+            Seconds = (UpdateTime / 1000).toInt()
+
+            Minutes = Seconds / 60
+
+            Seconds = Seconds % 60
+
+
+            if (Minutes.toString().length < 2) {
+                minute?.text = "0" + Minutes.toString()
+            } else {
+                minute?.text = Minutes.toString()
+            }
+            if (Seconds.toString().length < 2) {
+                seconds?.text = "0" + Seconds.toString()
+            } else {
+                seconds?.text = Seconds.toString()
+            }
+
+            milli_seconds?.text = MilliSeconds.toString()
+
+            handler?.postDelayed(this, 0)
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/aisyah/learnapp/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/aisyah/learnapp/ui/fragment/AccountFragment.kt
@@ -1,31 +1,28 @@
 package com.aisyah.learnapp.ui.fragment
 
-import android.app.Activity.RESULT_OK
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
 import com.aisyah.learnapp.R
 import com.aisyah.learnapp.SplashScreen
 import com.aisyah.learnapp.account.AboutAppActivity
 import com.aisyah.learnapp.account.EditAccountActivity
+import com.github.dhaval2404.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.fragment_account.*
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 
 class AccountFragment : Fragment() {
+
+    private var mProfileUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,11 +54,54 @@ class AccountFragment : Fragment() {
             }
         }
 
+        ivProfile.setOnClickListener {
+            ImagePicker.with(this)
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)
+                .cropSquare()	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+
+        }
+
         btn_logout.setOnClickListener {
             val intent = Intent(context, SplashScreen::class.java)
             startActivity(intent)
         }
 
+    }
+
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            if (resultCode == Activity.RESULT_OK) {
+                //Image Uri will not be null for RESULT_OK
+                val fileUri = data?.data!!
+
+                mProfileUri = fileUri
+                ivProfile.setImageURI(fileUri)
+            } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val uri: Uri = data?.data!!
+
+            // Use Uri object instead of File to avoid storage permissions
+            ivProfile.setImageURI(uri)
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
